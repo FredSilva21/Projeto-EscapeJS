c8919be7dcf0f36613bbae94819753306ffa12d6
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-10 18:08:49 +0100

    MVC

---
+++ EscapeJS/controllers/questionController.js
@@ -0,0 +1,5 @@
+class QuestionController {
+    static createQuestion() {
+        
+    }
+}
---
+++ EscapeJS/models/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(name, email, age, gender, password, password_confirmation){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.password = password;
+        this.password_confirmation = password_confirmation;
+    }
+}
