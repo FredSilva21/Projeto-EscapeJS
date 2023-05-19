c6f0c73b14d9f29e271446fbd4484f6fafe97c6f
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-19 16:46:31 +0100

    Hard coded admin

--- models/user.js
+++ models/user.js
@@ -1,65 +1,61 @@
 export class User {
-  constructor(id,name, email, age, gender, password) {
-    this.id= id
+  constructor(id, name, email, dateOfBirth, gender, password) {
+    this.id = 1;
     this.name = name;
     this.email = email;
-    this.age = age;
+    this.dateOfBirth = dateOfBirth;
     this.gender = gender;
     this.password = password;
   }
 }
 
 // Admin ROOT Account
-/*
-export function init() {
-  userDoc.push();
-}
-*/
-export let userDoc=[]
 
-export function generateId(id){
-  id=userDoc.length+1
-  return id
+export function init() {
+  userDoc = localStorage.userDoc
+    ? JSON.parse(localStorage.userDoc)
+    : [
+        {
+          id: 1,
+          name: "admin",
+          email: "admin@email.com",
+          dateOfBirth: "01-01-2000",
+          gender: "Other",
+          password: "admin123",
+        },
+      ];
+}
+
+export let userDoc = [];
+
+export function generateId(id) {
+  id = userDoc.length + 1;
+  return id;
 }
 
 // Add new User
-export function createUser(
-  id,
-  name,
-  email,
-  age,
-  gender,
-  password,
-) {
-  userDoc.push(
-    new User(
-      generateId(id),
-      name,
-      email,
-      age,
-      gender,
-      password,
-    )
-  );
+export function createUser(id, name, email, age, gender, password) {
+  userDoc.push(new User(generateId(id), name, email, age, gender, password));
   localStorage.userDoc = JSON.stringify(userDoc);
-
 }
 
-export function calculateAge(dateOfBirth){
+export function calculateAge(dateOfBirth) {
   const currentDate = new Date();
   const birthDate = new Date(dateOfBirth);
-  
+
   let age = currentDate.getFullYear() - birthDate.getFullYear();
-  
+
   const currentMonth = currentDate.getMonth();
   const birthMonth = birthDate.getMonth();
   const currentDay = currentDate.getDate();
   const birthDay = birthDate.getDate();
-  
-  if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+
+  if (
+    currentMonth < birthMonth ||
+    (currentMonth === birthMonth && currentDay < birthDay)
+  ) {
     age--;
   }
-  
+
   return age;
 }
-
