c61ff71c7f196a841bcde011ea3e4d99e0ef033a
FredSilva21 <fredsilva43@gmail.com>
2023-05-19 15:35:32 +0100

    register functions almost done

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 export class User {
-  constructor(name, email, age, gender, password, password_confirmation) {
+  constructor(id,name, email, age, gender, password) {
+    this.id= id
     this.name = name;
     this.email = email;
     this.age = age;
     this.gender = gender;
     this.password = password;
-    this.password_confirmation = password_confirmation;
   }
 }
 
@@ -15,26 +15,51 @@ export function init() {
   userDoc.push();
 }
 */
+export let userDoc=[]
+
+export function generateId(id){
+  id=userDoc.length+1
+  return id
+}
 
 // Add new User
 export function createUser(
+  id,
   name,
   email,
   age,
   gender,
   password,
-  password_confirmation
 ) {
   userDoc.push(
     new User(
-      generateId(),
+      generateId(id),
       name,
       email,
       age,
       gender,
       password,
-      password_confirmation
     )
   );
   localStorage.userDoc = JSON.stringify(userDoc);
+
 }
+
+export function calculateAge(dateOfBirth){
+  const currentDate = new Date();
+  const birthDate = new Date(dateOfBirth);
+  
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+  
+  const currentMonth = currentDate.getMonth();
+  const birthMonth = birthDate.getMonth();
+  const currentDay = currentDate.getDate();
+  const birthDay = birthDate.getDate();
+  
+  if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+    age--;
+  }
+  
+  return age;
+}
+
--- public/js/register.js
+++ public/js/register.js
@@ -1,4 +1,4 @@
-import * as user from "../../models/user"
+import * as user from "../../models/user.js"
 
 const form = document.querySelector("form");
 
@@ -9,7 +9,7 @@ submit.addEventListener("click", (e) => {
 
   const name = document.getElementById("username").value;
   const email = document.getElementById("email").value;
-  const age = document.getElementById("age").value;
+  const age = document.getElementById("dateOfBirth").value;
   const gender = document.getElementById("gender").value;
   const pwd = document.getElementById("password").value;
   const confirmPwd = document.getElementById("confirm-password").value;
@@ -18,4 +18,124 @@ submit.addEventListener("click", (e) => {
     alert("Password must be equal.");
     return;
   }
+
+  if(user.userDoc.find(user => user.name === name)){
+    alert("Already have an account with the same username.")
+    return
+  };
+
+  if(user.userDoc.find(user => user.email === email)){
+    alert("Already have an account with the same email.")
+    return
+  };
+
+  user.createUser(user.generateId(),name,email,user.calculateAge(age),gender,pwd)
+
+  renderForm()
 });
+
+function renderForm(){
+  const anc=document.querySelector(".has_account a")
+  const form=document.querySelector("form")
+
+  if(anc.textContent=="Sign In"){
+    anc.addEventListener("click",(e)=>{
+      e.preventDefault()
+      form.innerHTML=`<h1>Sign In</h1>
+      <div class="form-input">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Name"
+          required
+        />
+      </div>
+
+      <div class="form-input">
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          required
+        />
+      </div>
+
+      <div class="form-input">
+        <button type="submit">Sign In</button>
+        <span class="has_account">
+          Don't have an account? <a href="./register.html"></a>Sign Up</a>
+        </span>
+      </div>`
+    })
+  }else if(anc.textContent=="Sign In"){
+    form.innerHTML=`<h1>Sign Up</h1>
+        <div class="form-input">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Name"
+            required
+          />
+        </div>
+
+        <div class="form-input">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div class="form-input">
+          <input
+            type="date"
+            name="dateOfBirth"
+            id="dateOfBirth"
+            placeholder="Date of Birth"
+            required
+          />
+        </div>
+
+        <div class="form-input">
+          <select name="gender" id="gender" required>
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+
+        <div class="form-input">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <div class="form-input">
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            placeholder="Confirm Password"
+            required
+          />
+        </div>
+
+        <div class="form-input">
+          <button type="submit">Sign Up</button>
+          <span class="has_account">
+              Already have an account?<a>Sign In</a>
+          </span>
+          </span>
+        </div>`
+  }
+}
