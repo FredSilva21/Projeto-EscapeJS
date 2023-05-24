9bc26ef0ba2c279f3c10015727b8fbb045ebeb82
FredSilva21 <fredsilva43@gmail.com>
2023-05-24 22:10:59 +0100

    room1 imagem map & init function to room class

--- models/room.js
+++ models/room.js
@@ -1,8 +1,28 @@
 export class Room {
+  id=0;
+  name="";
+  photo="";
+  levels=[]
+
   constructor(id, name, photo, levels) {
     this.id = id;
     this.name = name;
     this.photo = photo;
     this.levels = levels;
+  }
+}
+
+let rooms=[]
+
+export function init() {
+  if(rooms.length==0){
+    rooms=[
+      {
+        id: 1,
+        name: "Chapter 1:The Beggining",
+        photo:"../public/images/room1",
+        levels:[]
+      },
+    ];
   }
 }
