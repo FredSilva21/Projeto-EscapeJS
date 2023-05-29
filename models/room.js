65627cfc020931a6ebc41eb5cf0f3167e0897c86
xnm-sdksdk <n.mendonca@protonmail.com>
2023-05-29 14:15:36 +0100

    room model

--- models/room.js
+++ models/room.js
@@ -1,10 +1,10 @@
 import { generateId } from "./user";
 
 export class Room {
-  id=0;
-  name="";
-  photo="";
-  levels=[]
+  id = 0;
+  name = "";
+  photo = "";
+  levels = [];
 
   constructor(id, name, photo, levels) {
     this.id = id;
@@ -12,44 +12,57 @@ export class Room {
     this.photo = photo;
     this.levels = levels;
   }
+
+  // Methods
+
+  addLevel(level) {
+    this.levels.push(level);
+  }
+
+  removeLevel(levelName) {
+    for (let level in this.levels) {
+      if (this.levels[level].name === levelName) {
+        this.levels.splice(level, 1);
+        return;
+      }
+    }
+    throw Error(`Level ${levelName} not found`);
+  }
 }
 
 // Load Rooms from local storage
-export let roomDoc
+export let roomDoc;
 
 export function init() {
-  if(roomDoc.length==0){
-    roomDoc=[
+  if (roomDoc.length == 0) {
+    roomDoc = [
       {
         id: 1,
         name: "Chapter 1:The Beggining",
-        photo:"../public/images/room1",
-        levels:[]
+        photo: "../public/images/room1",
+        levels: [],
       },
       {
-        id:2,
-        name:"Chapter 2:Working Hard",
-        photo:"../public/images/room2",
-        levels:[]
+        id: 2,
+        name: "Chapter 2:Working Hard",
+        photo: "../public/images/room2",
+        levels: [],
       },
       {
-        id:3,
-        name:"Chapter 3:Final Bosses",
-        photo:"../public/images/room3",
-        levels:[]
-      }
+        id: 3,
+        name: "Chapter 3:Final Bosses",
+        photo: "../public/images/room3",
+        levels: [],
+      },
     ];
   }
 }
 
-export function addRoom(name,photo,levels){
-  const newRoom=new Room(
-    {
-      id:generateId(),
-      name:name,
-      photo:photo,
-      levels:levels
-    }
-  )
-  roomDoc.push(newRoom)
+export function addRoom(name, photo, levels) {
+  if (roomDoc.some((room) => room.name === name)) {
+    throw Error(`Room ${name} already exists!`);
+  } else {
+    roomDoc.push(new Room(generateId(id), name, photo, levels));
+    localStorage.roomDoc = JSON.stringify(roomDoc);
+  }
 }
