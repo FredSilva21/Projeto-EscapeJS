61a25b0fd1256b4ca25f6f6e6738116579a9a15f
FredSilva21 <fredsilva43@gmail.com>
2023-06-01 20:01:05 +0100

    ranking at first 5

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -4,31 +4,36 @@ user.init();
 renderTable();
 
 function renderTable() {
-  const table = document.querySelector(".rankingTable");
-  let position = 1;
-  let template = `
-    <tr>
-      <th>Position</th>
-      <th>Name</th>
-      <th>Score</th>
-      <th>Type</th>
-    </tr>`;
-
-  const userData = user.exportAllUsers();
-  const sortedData = userData.sort((a, b) => b.score - a.score); // Sort by score in descending order
-
-  for (const player of sortedData) {
-    template += `
+    const table = document.querySelector(".rankingTable");
+    let position = 1;
+    let template = `
       <tr>
-        <td>${position}º</td>
-        <td>${player.name}</td>
-        <td>${player.score}</td>
-        <td>${player.type}</td>
+        <th>Position</th>
+        <th>Name</th>
+        <th>Score</th>
+        <th>Type</th>
       </tr>`;
-
-    position++;
-  }
-
-  table.innerHTML = template;
+  
+    const userData = user.exportAllUsers();
+    const sortedData = userData.sort((a, b) => b.score - a.score); // Sort by score in descending order
+  
+    for (const player of sortedData) {
+      if (position > 5) {
+        break; // Exit the loop after displaying the top 5 players
+      }
+  
+      template += `
+        <tr>
+          <td>${position}º</td>
+          <td>${player.name}</td>
+          <td>${player.score}</td>
+          <td>${player.type}</td>
+        </tr>`;
+  
+      position++;
+    }
+  
+    table.innerHTML = template;
 }
+  
 
