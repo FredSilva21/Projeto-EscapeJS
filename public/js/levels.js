80bc94540e0932f300285ed269b2c95b8fe8907d
FredSilva21 <fredsilva43@gmail.com>
2023-06-11 12:41:42 +0100

    level

--- public/js/levels.js
+++ public/js/levels.js
@@ -1,2 +1,6 @@
 import * as Room from "../../models/room.js"
 import * as Question from "../../models/question.js" 
+
+function renderLevel(){
+    const levelContainer=document.querySelector(".level-container")
+}
