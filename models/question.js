9a8f445c1f8c60d8d81987a4e55ce6e4ce2af749
FredSilva21 <fredsilva43@gmail.com>
2023-06-14 21:32:37 +0100

    level 1 and level 2 questions

--- models/question.js
+++ models/question.js
@@ -54,6 +54,72 @@ export function init() {
           solution: "False",
           solved: 0,
         },
+        {
+          id: 5,
+          name: "What's the result?",
+          image: "../public/images/level1-question5.png",
+          options:["0", "1", "2", "3"],
+          solution: "1",
+          solved: 0,
+        },
+        {
+          id: 6,
+          name: "The JavaScript 'typeof' operator can be used to determine the data type of a variable.",
+          image: "",
+          options:["True", "False"],
+          solution: "True",
+          solved: 0,
+        },
+        //Level 2
+        {
+          id: 7,
+          name: "What will be the output of the following code?",
+          image: "../public/images/level2-question1.png",
+          options:["Greater than 5","Equal to 5","Less than 5","undefined"],
+          solution: "Greater than 5",
+          solved: 0,
+        },
+        {
+          id: 8,
+          name: "What is the error in the following code?",
+          image: "../public/images/level2-question2.png",
+          options:["Missing semicolon at the end of the first line","Division by zero","Undeclared variable 'result'","Missing 'catch' block"],
+          solution: "Greater than 5",
+          solved: 0,
+        },
+        {
+          id: 9,
+          name: "The if-else statement allows the execution of different code blocks based on specified conditions.",
+          image: "",
+          options:["True", "False"],
+          solution: "True",
+          solved: 0,
+        },
+        {
+          id: 10,
+          name: "What will be the output of the following code?",
+          image: "../public/images/level2-question4.png",
+          options:["It's a hot and sunny day!","The weather is not hot and sunny."],
+          solution: "The weather is not hot and sunny.",
+          solved: 0,
+        },
+        //Level 3
+        {
+          id: 8,
+          name: "What is the error in the following code??",
+          image: "../public/images/level2-question1.png",
+          options:["Missing semicolon at the end of the first line","Division by zero","Undeclared variable 'result'","Missing 'catch' block"],
+          solution: "Greater than 5",
+          solved: 0,
+        },
+        {
+          id: 8,
+          name: "What is the error in the following code??",
+          image: "../public/images/level2-question1.png",
+          options:["Missing semicolon at the end of the first line","Division by zero","Undeclared variable 'result'","Missing 'catch' block"],
+          solution: "Greater than 5",
+          solved: 0,
+        },
       ];
 
   localStorage.setItem("questionsDoc", JSON.stringify(questionsDoc));
