4726f7a4c2777deb0b1c94f525c69de8e2ec2e17
xnm-sdksdk <n.mendonca@protonmail.com>
2023-06-14 14:08:36 +0100

    not finding questions

--- public/js/levels.js
+++ public/js/levels.js
@@ -1,14 +1,14 @@
-import * as Room from "../../models/room.js"
-import * as Question from "../../models/question.js" 
+import * as Room from "../../models/room.js";
+import * as Question from "../../models/question.js";
 
-Room.init()
+Room.init();
 //Get variable in url
 const urlParams = new URLSearchParams(window.location.search);
-const levelId = urlParams.get('levelId');
+const levelId = urlParams.get("levelId");
 
 let level;
 for (const room of Room.roomDoc) {
-  level = room.levels.find(level => level.id == levelId);
+  level = room.levels.find((level) => level.id == levelId);
   if (level) {
     break;
   }
@@ -18,39 +18,55 @@ for (const room of Room.roomDoc) {
 const title = document.querySelector("h3");
 title.innerHTML = `Level ${levelId}-${level.name}`;
 
-function renderLevel(){
-    const questionTitle=document.querySelector(".question-title")
-    const questionOptions=document.querySelector(".question-options")
+function renderLevel() {
+  const questionTitle = document.querySelector(".question-title");
+  const questionOptions = document.querySelector(".question-options");
 
-    // Get the level object
-    const roomLevel = Room.roomDoc.find(room => room.levels.some(level => level.id == levelId));
-  
-    if (!roomLevel) {
-      console.log("Level not found.");
-      return;
-    }
+  // Get the level object
+  const roomLevel = Room.roomDoc.find((room) =>
+    room.levels.some((level) => level.id == levelId)
+  );
 
-    // Get a random level from the room's levels array
-    const level = roomLevel.levels[Math.floor(Math.random() * roomLevel.levels.length)];
-    console.log(level)
+  if (!roomLevel) {
+    console.log("Level not found.");
+    return;
+  }
 
-    // Get a random question from the level's questions array
-    const randomQuestion = level.questions[Math.floor(Math.random() * level.questions.length)];
-    console.log(randomQuestion)
+  // Get a random level from the room's levels array
+  const level =
+    roomLevel.levels[Math.floor(Math.random() * roomLevel.levels.length)];
+  console.log(level);
 
-    // Set the question title
-    questionTitle.innerHTML = randomQuestion.name;
+  if (!level.questions || level.questions.length === 0) {
+    console.log("No questions found.");
+    return
+  }
 
-    // Clear previous question options
-    questionOptions.innerHTML = "";
+  // Get a random question from the level's questions array
+  const randomQuestion =
+    level.questions[Math.floor(Math.random() * level.questions.length)];
+  console.log(randomQuestion);
 
-    // Shuffle the options randomly
-    const shuffledOptions = randomQuestion.options.sort(() => Math.random() - 0.5);
+  if (!randomQuestion) {
+    console.log("Question name is undefined");
+    return
+  }
 
-    // Loop through the options and add them to the questionOptions element
-    shuffledOptions.forEach(option => {
-        questionOptions.innerHTML=`<button class="option-button" value="${option}">${option}</button>`
-    });
+  // Set the question title
+  questionTitle.innerHTML = randomQuestion.name;
+
+  // Clear previous question options
+  questionOptions.innerHTML = "";
+
+  // Shuffle the options randomly
+  const shuffledOptions = randomQuestion.options.sort(
+    () => Math.random() - 0.5
+  );
+
+  // Loop through the options and add them to the questionOptions element
+  shuffledOptions.forEach((option) => {
+    questionOptions.innerHTML += `<button class="option-button" value="${option}">${option}</button>`;
+  });
 }
-  
-renderLevel()
+
+renderLevel();
